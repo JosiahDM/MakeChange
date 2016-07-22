365b1efefd9fd33705a71896c17b0296ed895390
Josiah Moye <jmoye@neo.rr.com>
2016-07-22 14:31:06 -0600

    Added some initial stub methods

--- src/change/CashRegister.java
+++ src/change/CashRegister.java
@@ -4,4 +4,22 @@ public class CashRegister {
 	public static void main(String[] args) {
 		
 	}
+	
+	// The user is prompted asking for the price of the item.
+	static double promptPrice() {
+		return 0;
+	}
+	
+	//The user is prompted asking how much 
+	// money was tendered by the customer.
+	static double promptMoneyTendered() {
+		return 0;
+	}
+	
+	// Display an appropriate message if the customer provided 
+	// too little money or the exact amount.
+	static boolean validAmount() {
+		return false;
+	}
+	
 }
